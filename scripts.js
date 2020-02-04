1372fafe60be03e76bc82d1da4fb233900001960
Francisco J Cortez <cortez.francisco.j.2009@gmail.com>
2020-02-04 12:34:45 -0600

    Created basic template for inserting pokemon pic and pokemon name.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,30 @@
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
-        "x-rapidapi-key": "api-page-endpoints_form-param_mashape-key_default"
-    }
-}
+//Used for gettig random object drink
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
+// var settings = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": "https://the-cocktail-db.p.rapidapi.com/random.php",
+//     "method": "GET",
+//     "headers": {
+//         "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+//         "x-rapidapi-key": "0dd8aec848msh789cce3c0ec7bd5p1717d6jsn46bdb806c904"
+//     }
+// }
+
+// $.ajax(settings).done(function (response) {
+//     console.log(response);
+// });
+
+var randomPokeIndex = Math.floor((Math.random() * 151) + 1)
+var tempPokemon = "https://pokeapi.co/api/v2/pokemon/" + randomPokeIndex;
+
+
+$.ajax({
+  url: tempPokemon,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+
+    $("#pic").attr("src", response.sprites.front_default);
+    $("#name").text(response.name);
 });
